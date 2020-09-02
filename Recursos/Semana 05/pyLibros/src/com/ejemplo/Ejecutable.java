947743a4ffd6c924fe7f6f8932976ea69161dc1c
martindapol <martindapol@gmail.com>
2020-09-02 12:17:34 -0300

    Biblioteca y Libro

--- Recursos/Semana 05/pyLibros/src/com/ejemplo/Ejecutable.java
+++ Recursos/Semana 05/pyLibros/src/com/ejemplo/Ejecutable.java
@@ -5,6 +5,7 @@
  */
 package com.ejemplo;
 
+import com.ejemplo.dominio.Biblioteca;
 import com.ejemplo.dominio.Libro;
 import java.util.Scanner;
 
@@ -14,12 +15,18 @@ import java.util.Scanner;
  */
 public class Ejecutable {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in); // para cadenas
         Scanner entrada2 = new Scanner(System.in); // para otros tipos de variables
+
+        int cant;
+        do {
+            System.out.println("Ingrese la cantidad de libros de la biblioteca: (>0) ");
+            cant = entrada2.nextInt();
+        } while (cant <= 0);
+
+        //Crear un objeto biblioteca:
+        Biblioteca miBiblioteca = new Biblioteca(cant);
 
         // Libro creado con valores por defecto:
         Libro libro1, libro2; // son objetos? son referencias valen null;
@@ -27,8 +34,6 @@ public class Ejecutable {
         libro1.setIsbn("324-343444-44");
         libro1.setAutor("Otro autor");
         libro1.setPaginas(500);
-        
-        
 
         //Libreo creado con valores ingresados por teclado:
         System.out.println("Ingrese ISBN del libro: ");
@@ -45,17 +50,17 @@ public class Ejecutable {
 
         libro2 = new Libro(isbn, autor, tit, paginas);
 
-        //Resultados:
-        System.out.println("Libro 1: " + libro1);
-        System.out.println("Libro 2: " + libro2.toString());
-       
-        if (libro1.getPaginas() > libro2.getPaginas()) {
-            System.out.println(libro1.getTitulo() +  " con más páginas!");
-        } else if (libro2.getPaginas() > libro1.getPaginas()) {
-            System.out.println(libro2.getTitulo() +  " tiene más páginas!");
+        if (miBiblioteca.agregarLibro(libro1) == true) {
+            System.out.println("Libro agregado!");
         } else {
-            System.out.println("Ambos libros tienen la misma cantidad de páginas!");
+            System.out.println("No se pudo agregar el libro!");
         }
-    }
 
+        //simplemente lo agregamos sin validar:
+        miBiblioteca.agregarLibro(libro2);
+
+        //Mostrar estado de la biblioteca:
+        String listado = miBiblioteca.mostrarListado();
+        System.out.println(listado);
+    }
 }
