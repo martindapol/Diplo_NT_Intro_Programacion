7d99889faf8acb4ef9c92b35f8f3d8c498f22f59
martindapol <martindapol@gmail.com>
2020-09-14 20:33:15 -0300

    Proyecto Multas con GUI

---
+++ Recursos/Semana 07/pyMultas_4/src/com/ejemplo/Ejecutable.java
@@ -0,0 +1,17 @@
+package com.ejemplo;
+
+import com.ejemplo.gui.JFPrincipal;
+
+/**
+ *
+ * @author MARTIN
+ */
+public class Ejecutable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new JFPrincipal("Multas Caminera").setVisible(true);
+    }
+}
