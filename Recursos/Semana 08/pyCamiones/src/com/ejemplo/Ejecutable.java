0e55ce0c8b24515d802052cbceaa9fb9558aa702
martindapol <martindapol@gmail.com>
2020-09-17 22:47:37 -0300

    Camiones con GUI

--- Recursos/Semana 08/pyCamiones/src/com/ejemplo/Ejecutable.java
+++ Recursos/Semana 08/pyCamiones/src/com/ejemplo/Ejecutable.java
@@ -5,6 +5,8 @@
  */
 package com.ejemplo;
 
+import com.ejemplo.gui.JFPrincipal;
+
 /**
  *
  * @author MARTIN
@@ -15,7 +17,7 @@ public class Ejecutable {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new JFPrincipal().setVisible(true);
     }
     
 }
